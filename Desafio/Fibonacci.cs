6e6e30f2783ae327e60d51a231d5af67deb48c89
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2023-02-23 15:47:55 -0300

    Add project files.

---
+++ Desafio/Fibonacci.cs
@@ -0,0 +1,17 @@
+﻿namespace Desafio
+{
+    public static class Fibonacci
+    {
+        public static int[] Sequencia(int numero)
+        {
+            int[] fibonacci = new int[numero];
+            fibonacci[0] = 1;
+            fibonacci[1] = 1;
+            for (int i = 2; i < numero; i++)
+            {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            }
+            return fibonacci;
+        }
+    }
+}
---
+++ Desafio/Program.cs
@@ -0,0 +1,11 @@
+﻿using Desafio;
+
+Console.WriteLine("Digite o número de Iterações");
+int iteracoes = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int[] fibonacci = Fibonacci.Sequencia(iteracoes);
+for (int i = 0; i < iteracoes; i++)
+{
+    Console.WriteLine(string.Join(" ", fibonacci.Take(i + 1)));
+}
